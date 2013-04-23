b781cb0cf25d6bee9cc2749397eaa66566e60da4
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2013-04-22 19:21:55 -0500

    Add more interface code for close, dispatcher and custom user/pass verify

--- src/authy-openvpn.c
+++ src/authy-openvpn.c
@@ -10,8 +10,10 @@
 #define false 0
 
 
-/* Pending define context, that is the keep state */
+/* Pending: define context, a.k.a state */
 
+struct plugin_context {
+};
 
 /*
  * Given an environmental variable name, search
@@ -37,4 +39,51 @@ get_env (const char *name, const char *envp[])
         }
     }
   return NULL;
+}
+
+/*
+ * Plugin initialization
+ */
+OPENVPN_EXPORT openvpn_plugin_handle_t
+openvpn_plugin_open_v1 (unsigned int *type_mask, const char *argv[],
+                        const char *envp[])
+{
+  /* Context Allocation */
+
+  /* Set type_mask, a.k.a callbacks that we want to intercept */
+  *type_mask = OPENVPN_PLUGIN_MASK(OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY);
+    
+  /* Cast and return the context */
+}
+
+static int
+authenticate(plugin_context *context, const char *argv[], const char *envp)
+{
+  /* doit */
+}
+
+/*
+ * Dispatcher
+ */
+OPENVPN_EXPORT int
+openvpn_plugin_func_v1 (openvpn_plugin_handle_t handle, const int type,
+			const char *argv[], const char *envp[])
+{
+
+  /* Pending: set/cast handle to context */
+  
+  if(type == OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY)
+    return authenticate(context, argv envp);
+  return OPENVPN_PLUGIN_FUNC_ERROR; /* Not sure, but for now it should
+                                       be an error if we handle other callbacks */
+}
+
+/*
+ * Free the memory related with the context
+ */
+OPENVPN_EXPORT void
+openvpn_plugin_close_v1 (openvpn_plugin_handle_t handle)
+{
+  /* struct plugin_context *context = (struct plugin_context *) handle; */
+  /* free (context); */
 }
