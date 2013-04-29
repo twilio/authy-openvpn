70c63e53f55fcaa15d5d1c72aefa47b8f10c33c9
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2013-04-29 12:05:01 -0500

    Update authy wrapper

--- src/authy_api.c
+++ src/authy_api.c
@@ -1,8 +1,5 @@
 #include "authy_api.h"
 
-#define NSizeAux strlen("?api_key=")
-#define ALLOCURL (char *) malloc(strlen(pszAPIUrl) + strlen(pszEndPoint) +NSizeAux + strlen(pszAPIKey) + 1)
-
 static size_t
 custom_writer(char *ptr, size_t size, size_t nmemb,
               void *userdata)
@@ -13,96 +10,111 @@ custom_writer(char *ptr, size_t size, size_t nmemb,
 }
 
 static int
-curl_request(char *pszUrl, char *pszPostFields, char *pszResponse)
+curl_request(char *psz_url, char *psz_post_fields, char *psz_response)
 {
-  CURL *pCurl;
-  int iRes;
+  CURL *p_curl;
+  int i_res;
 
   curl_global_init(CURL_GLOBAL_ALL);
 
-  pCurl = curl_easy_init();
+  p_curl = curl_easy_init();
 
-  iRes = -1;
+  i_res = -1;
 
-  if(pCurl)
+  if(p_curl)
     {
-      curl_easy_setopt(pCurl, CURLOPT_URL, pszUrl);
+      curl_easy_setopt(p_curl, CURLOPT_URL, psz_url);
 
-      if(pszPostFields)
-        curl_easy_setopt(pCurl, CURLOPT_POSTFIELDS, pszPostFields);
+      if(psz_post_fields)
+        curl_easy_setopt(p_curl, CURLOPT_POSTFIELDS, psz_post_fields);
 
       /* This option determines whether curl verifies the authenticity
          of the peer's certificate */
-      curl_easy_setopt(pCurl, CURLOPT_SSL_VERIFYPEER, 1L);
+      curl_easy_setopt(p_curl, CURLOPT_SSL_VERIFYPEER, 1L);
       /* curl_easy_setopt(pCurl, CURLOPT_VERBOSE, 1L); */
 
-      curl_easy_setopt(pCurl, CURLOPT_WRITEFUNCTION, custom_writer);
-      curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, pszResponse);
+      curl_easy_setopt(p_curl, CURLOPT_WRITEFUNCTION, custom_writer);
+      curl_easy_setopt(p_curl, CURLOPT_WRITEDATA, psz_response);
 
-      iRes = (int) curl_easy_perform(pCurl);
+      i_res = (int) curl_easy_perform(p_curl);
       /* Always cleanup otherwise it will lead to memoryleaks */
-      curl_easy_cleanup(pCurl);
+      curl_easy_cleanup(p_curl);
     }
-  return iRes;
+  return i_res;
 }
 
 /* It returns the actual size of the url */
 
 static int
-url_builder(const char *pszAPIUrl, const char *pszAPIKey,
-            char *pszEndPoint, char *pszUrl)
+url_builder(const char *psz_API_url, const char *psz_API_key,
+            char *psz_end_point, char *psz_url)
 {
-  return sprintf(pszUrl, "%s%s?api_key=%s", pszAPIUrl, pszEndPoint, pszAPIKey);
+  return sprintf(psz_url, "%s%s?api_key=%s", psz_API_url, psz_end_point, psz_API_key);
+}
+
+/* returns the needed size to built the url */
+static int
+url_size(const char *psz_API_url, const char *psz_end_point,
+         const char *psz_API_key)
+{
+  return strlen(psz_API_url) + strlen(psz_end_point) +
+    strlen("?api_key=") + strlen(psz_API_key)   + 1;
 }
 
 extern int
-register_user(const char *pszAPIUrl, const char *pszAPIKey,
-              char *pszPostFields, char *pszResponse)
+register_user(const char *psz_API_url, const char *psz_API_key,
+              char *psz_post_fields, char *psz_response)
 {
-  int iRes;
-  char *pszUrl, *pszEndPoint = "/users/new";
+  int i_res;
+  char *psz_url, *psz_end_point = "/users/new";
 
-  pszUrl = ALLOCURL;
-  url_builder(pszAPIUrl, pszAPIKey, pszEndPoint, pszUrl);
+  psz_url = (char *) malloc(url_size(psz_API_url, psz_end_point,
+                                     psz_API_key));
 
-  iRes = curl_request(pszUrl, pszPostFields, pszResponse);
-  free(pszUrl);
-  return iRes;
+  url_builder(psz_API_url, psz_API_key, psz_end_point, psz_url);
+
+  i_res = curl_request(psz_url, psz_post_fields, psz_response);
+  free(psz_url);
+  return i_res;
 }
 
 extern int
-verify(const char *pszAPIUrl, const char *pszAPIKey,
-       char *pszToken, char *pszAuthyID, char *pszResponse)
+verify(const char *psz_API_url, const char *psz_API_key,
+       char *psz_token, char *psz_authy_ID, char *psz_response)
 {
-  int iRes;
-  char *pszUrl, *pszEndPoint;
-  pszEndPoint = (char *) malloc(strlen("/verify/") + strlen(pszToken)
-                                + strlen("/") + strlen(pszAuthyID));
-  sprintf(pszEndPoint, "/verify/%s/%s", pszToken, pszAuthyID);
+  int i_res;
+  char *psz_url, *psz_end_point;
+  psz_end_point = (char *) malloc(strlen("/verify/") + strlen(psz_token)
+                                  + strlen("/") + strlen(psz_authy_ID));
+  sprintf(psz_end_point, "/verify/%s/%s", psz_token, psz_authy_ID);
 
-  pszUrl = ALLOCURL;
-  url_builder(pszAPIUrl, pszAPIKey, pszEndPoint, pszUrl);
+  psz_url = (char *) malloc(url_size(psz_API_url, psz_end_point,
+                                     psz_API_key));
 
-  iRes = curl_request(pszUrl, NULL, pszResponse);
-  free(pszUrl);
-  free(pszEndPoint);
-  return iRes;
+  url_builder(psz_API_url, psz_API_key, psz_end_point, psz_url);
+
+  i_res = curl_request(psz_url, NULL, psz_response);
+  free(psz_url);
+  free(psz_end_point);
+  return i_res;
 }
 
 extern int
-request_sms(const char *pszAPIUrl, const char *pszAPIKey,
-            char *pszAuthyID, char *pszResponse)
+request_sms(const char *psz_API_url, const char *psz_API_key,
+            char *psz_authy_ID, char *psz_response)
 {
-  int iRes;
-  char *pszUrl, *pszEndPoint;
-  pszEndPoint = (char *) malloc(strlen("/sms/") + strlen(pszAuthyID));
-  sprintf(pszEndPoint, "/sms/%s", pszAuthyID);
+  int i_res;
+  char *psz_url, *psz_end_point;
+  psz_end_point = (char *) malloc(strlen("/sms/") + strlen(psz_authy_ID));
+  sprintf(psz_end_point, "/sms/%s", psz_authy_ID);
 
-  pszUrl = ALLOCURL;
-  url_builder(pszAPIUrl, pszAPIKey, pszEndPoint, pszUrl);
+  psz_url = (char *) malloc(url_size(psz_API_url, psz_end_point,
+                                     psz_API_key));
 
-  iRes = curl_request(pszUrl, NULL, pszResponse);
-  free(pszUrl);
-  free(pszEndPoint);
-  return iRes;
+  url_builder(psz_API_url, psz_API_key, psz_end_point, psz_url);
+
+  i_res = curl_request(psz_url, NULL, psz_response);
+  free(psz_url);
+  free(psz_end_point);
+  return i_res;
 }
--- src/authy_api.h
+++ src/authy_api.h
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern int register_user(const char *pszAPIUrl, const char *pszAPIKey,
-                         char *pszPostFields, char *pszResponse);
+extern int register_user(const char *psz_API_url, const char *psz_API_key,
+                         char *psz_post_fields, char *psz_response);
 
-extern int verify(const char *pszAPIUrl, const char *pszAPIKey,
-                  char *pszToken, char *pszAuthyID, char *pszResponse);
+extern int verify(const char *psz_API_url, const char *psz_API_key,
+                  char *psz_token, char *psz_authy_ID, char *psz_response);
 
-extern int request_sms(const char *pszAPIUrl, const char *pszAPIKey,
-                       char *pszAuthyID, char *pszResponse);
+extern int request_sms(const char *psz_API_url, const char *psz_API_key,
+                       char *psz_authy_ID, char *psz_response);
