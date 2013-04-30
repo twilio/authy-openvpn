9b556e2528126aa41d264e71e9db7d93728e50c0
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2013-04-30 10:43:45 -0500

    start debianization

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(authy-open-vpn):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="authy-open-vpn" command="/usr/bin/authy-open-vpn"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+libauthy-open-vpn 1.0 authy-open-vpn (>> 1.0-0), authy-open-vpn (<< 1.0-99)
